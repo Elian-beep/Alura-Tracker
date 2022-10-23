209a6ea2bb4a5411f5e92e44ffb9d484fe0cd32b
Elian-beep <elianoliver101@gmail.com>
2022-10-22 22:13:54 -0300

    created left bar and form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <HelloWorld/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioPrincipal />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BarraLateral from '@/components/BarraLateral.vue';
+import FormularioPrincipal from '@/components/FormularioPrincipal.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    BarraLateral,
+    FormularioPrincipal
+}
 });
 </script>
+
+
+<style>
+</style>
