5d3cf3e5e0a5d2a221ac0194fbffe84e132e852a
Elian-beep <elianoliver101@gmail.com>
2022-10-22 21:01:20 -0300

    create project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>
+    Ola mundo
+  </h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+});
+</script>
